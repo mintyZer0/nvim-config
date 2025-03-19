fb25029e5acf259ea3fbe1b045e922eb3de3919d
minty <eithanmathewmalonzo@gmail.com>
2025-03-20 00:18:00 +0800

    added lsp

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+vim.cmd("set number")
 vim.opt.clipboard = "unnamedplus"
 require("config.lazy")
 local builtin = require('telescope.builtin')
