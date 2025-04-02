84af02827f1897dc33dcf31583e645c7eefc05eb
minty <eithanmathewmalonzo@gmail.com>
2025-04-02 10:10:16 +0800

    added null-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return{
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "pyright"}
+                ensure_installed = {"lua_ls","basedpyright"}
 
             })
         end
@@ -18,9 +18,9 @@ return{
        "neovim/nvim-lspconfig",
        config = function()
            local lspconfig = require("lspconfig")
-           lspconfig.lua_ls.setup({})
-           lspconfig.pyright.setup({})
-           vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+            lspconfig.lua_ls.setup({})
+            lspconfig.basedpyright.setup({})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
        end
     }
 }
